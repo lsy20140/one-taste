28c64d50795bd76bbea3d14d42bf14323057a371
lsy20140 <lsy20140@naver.com>
2024-04-26 15:47:31 +0900

    design: placeholder, onBlur prop 추가 및 디자인 수정(bgColor, shadow, rounded)

--- src/components/common/Input/index.tsx
+++ src/components/common/Input/index.tsx
@@ -2,14 +2,18 @@
 import React from 'react'
 
 type Props = {
-  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void
+  placeholder: string,
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void,
+  onBlur?: () => void
 }
 
-export default function Input({onChange}: Props) {
+export default function Input({placeholder, onChange, onBlur}: Props) {
   return (
     <input 
-      className='w-full h-full px-3 bg-neutral-100 rounded-lg outline-none'
+      className='w-full h-full px-4 bg-white border border-neutral-300 shadow-sm rounded-full outline-none'
+      placeholder={placeholder}
       onChange={onChange}
+      onBlur={onBlur}
     />
   )
 }
