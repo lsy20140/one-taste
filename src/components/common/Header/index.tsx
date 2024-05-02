a87e90422de07a7540b450ac4c4324019583e018
lsy20140 <lsy20140@naver.com>
2024-05-02 23:54:17 +0900

    design: Header 반응형 디자인 적용

--- src/components/common/Header/index.tsx
+++ src/components/common/Header/index.tsx
@@ -12,32 +12,42 @@ export default function Header() {
 
   return (
     <>
-    <header className='sticky top-0 bg-white px-24 max-md:px-16 max-sm:px-4 border-b z-30'>
+    <header className='sticky top-0 bg-white px-24 max-md:px-16 max-sm:px-9 border-b z-30'>
       <div className="h-16 flex justify-between py-3">
         {/* 로고 반응형 */}
-        <div className='relative w-1/6 flex flex-grow-1 h-full max-sm:hidden'>
+        <div className='relative w-1/3 flex flex-grow-1 h-full max-sm:hidden'>
           <Link href={'/'} className='flex items-center'>
             <Image alt='logo' src={'/images/logo.svg'} width={150} height={24}/>
           </Link>
         </div>
-        <div className='relative w-1/6 h-full sm:hidden'>
-          <Link href={'/'}>
-            <Image alt='logo' src={'/images/icon.svg'} fill/>
+        <div className='relative w-1/3 h-full flex justify-start sm:hidden'>
+          <Link href={'/'} className='flex items-center'>
+            <Image alt='logo' src={'/images/icon.svg'} width={44} height={44}/>
           </Link>
         </div>
         {/* 검색 form, 자동 완성 wrapper */}
-        <div className='h-full w-1/3 text-center flex-grow-2'>
+        <div className='h-full w-1/3 max-sm:w-2/3 text-center flex-grow-2'>
           <SearchPlace />
         </div>
         {/* session 존재 여부로 구분 */}
-        <div className='w-1/6 flex justify-end items-center'>
+        <div className='w-1/3 flex justify-end items-center'>
           {user ? 
-            <div className='flex gap-4 justify-end w-full items-center'>
+          <>
+            <div className='flex gap-4 justify-end w-full items-center max-lg:hidden'>
+              <Image src={user.image ?? ""} width={40} height={40} alt={user.nickname} className='rounded-full'/>
               <p className='font-semibold shrink-0'>{user.nickname}님</p>
-              <Button text="로그아웃" onClick={() => signOut()} color='black' size='small'/>
+              <div className='w-28 shrink-0'>
+                <Button text="로그아웃" onClick={() => signOut()} color='black' size='small'/>
+              </div>
             </div>
+            <div className='lg:hidden'>
+              <Image src={user.image ?? ""} width={40} height={40} alt={user.nickname} className='rounded-full'/>
+            </div>
+          </>
           :
-            <Button text="로그인" onClick={() => signIn()}/>     
+            <div className='w-48'>
+              <Button text="로그인" onClick={() => signIn()}/>   
+            </div>      
           }
         </div>
       </div>
