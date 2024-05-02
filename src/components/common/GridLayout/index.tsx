e544a456f5579a0f1ea162b80f3fbed6fc327646
lsy20140 <lsy20140@naver.com>
2024-05-02 23:59:56 +0900

    design: GridLayout 화면 크기별 grid-cols  설정하여 반응형 디자인 적용

--- src/components/common/GridLayout/index.tsx
+++ src/components/common/GridLayout/index.tsx
@@ -1,7 +1,7 @@
 export default function GridLayout({children}: {children: React.ReactNode}) {
   return (
     <>
-      <div className="grid grid-cols-4 gap-4 m-auto justify-items-center">
+      <div className="grid grid-cols-4 gap-4 max-md:grid-cols-3 max-sm:grid-cols-2 m-auto justify-items-center">
         {children}
       </div>
     </>
