7e11921f9c665e6f681578a5374643825c7d2a3f
lsy20140 <lsy20140@naver.com>
2024-05-14 18:27:55 +0900

    fix: Input 컴포넌트 속성 수정

--- src/components/common/Input/index.tsx
+++ src/components/common/Input/index.tsx
@@ -3,15 +3,19 @@ import React from 'react'
 
 type Props = {
   placeholder: string,
+  value?: string,
+  maxLength?: number,
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void,
   onBlur?: () => void
 }
 
-export default function Input({placeholder, onChange, onBlur}: Props) {
+export default function Input({placeholder, value, maxLength, onChange, onBlur}: Props) {
   return (
     <input 
-      className='w-full h-full px-4 bg-white border border-neutral-300 shadow-sm rounded-full outline-none'
+      className='w-full h-full py-2 px-4 bg-white border border-neutral-300 shadow-sm rounded-full outline-none'
       placeholder={placeholder}
+      value={value ?? ""}
+      maxLength={maxLength}
       onChange={onChange}
       onBlur={onBlur}
     />
