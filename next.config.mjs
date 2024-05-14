c0981948e84aeae25fcc80d2f9a5a03929fc110a
lsy20140 <lsy20140@naver.com>
2024-05-14 18:24:09 +0900

    chore: Google 소셜 로그인 시 프로필 이미지 보여주기 위해 image domain 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: false,
   images: {
-    domains: ["one-taste.s3.ap-northeast-2.amazonaws.com"]
-  }
+    domains: [
+      "one-taste.s3.ap-northeast-2.amazonaws.com",
+      "lh3.googleusercontent.com"
+    ]
+  },
 };
 
 export default nextConfig;
