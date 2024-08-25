5b364e24976c95a1cd3c316ee7524d9189a8c916
lsy20140 <lsy20140@naver.com>
2024-08-25 23:17:23 +0900

    design: Button, Input 컴포넌트 세부 수정 사항 반영

--- src/components/common/Button/index.tsx
+++ src/components/common/Button/index.tsx
@@ -1,39 +1,54 @@
-'use client'
+"use client";
 
 type Props = {
-  children: React.ReactNode,
-  onClick?: (e:React.MouseEvent<HTMLElement, MouseEvent>) => void,
-  bgColor?: 'black' | 'red' | string,
-  textColor? : 'black' | 'white' | string,
-  size?: 'small' | 'normal'
-}
+  children: React.ReactNode;
+  onClick?: (e: React.MouseEvent<HTMLElement, MouseEvent>) => void;
+  bgColor?: "black" | "red" | "disabled" | string;
+  textColor?: "black" | "white" | string;
+  size?: "small" | "normal";
+};
 
-export default function Button({children, onClick, bgColor, textColor = 'black', size='normal'}: Props) {
-  const baseStyle = 'w-full rounded-lg font-semibold tracking-wide'
+export default function Button({
+  children,
+  onClick,
+  bgColor,
+  textColor = "black",
+  size = "normal",
+}: Props) {
+  const baseStyle = "w-full rounded-lg font-semibold tracking-wide";
   return (
-    <button className={`${baseStyle} ${getColorStyle(bgColor, textColor)} ${getPaddingStyle(size)}`} onClick={onClick}>
+    <button
+      className={`${baseStyle} ${getColorStyle(
+        bgColor,
+        textColor
+      )} ${getPaddingStyle(size)}`}
+      onClick={onClick}
+    >
       {children}
     </button>
-  )
+  );
 }
 
 const getPaddingStyle = (size?: string) => {
-  let paddingStyle = 'p-2'
+  let paddingStyle = "p-2";
 
-  switch(size) {
-    case 'small':
-      paddingStyle='py-2 px-6'
-      break
+  switch (size) {
+    case "small":
+      paddingStyle = "py-2 px-6";
+      break;
   }
 
-  return paddingStyle
-}
+  return paddingStyle;
+};
 
-const getColorStyle = (bgColor?: string, textColor?: string) =>{
-  let colorStyle = 'bg-neutral-200'
-  if(bgColor){
-    colorStyle=`bg-${bgColor} text-${textColor}`
+const getColorStyle = (bgColor?: string, textColor?: string) => {
+  let colorStyle = "bg-neutral-200";
+  if (bgColor === "black") {
+    colorStyle = `bg-black text-${textColor}`;
+  } else if (bgColor === "red") {
+    colorStyle = `bg-red-500 text-${textColor}`;
+  } else if (bgColor === "disabled") {
+    colorStyle = `bg-gray-500 text-${textColor} cursor-default`;
   }
-  
-  return colorStyle
-}
+  return colorStyle;
+};
--- src/components/common/Input/index.tsx
+++ src/components/common/Input/index.tsx
@@ -1,23 +1,29 @@
-'use client'
-import React from 'react'
+"use client";
+import React from "react";
 
 type Props = {
-  placeholder: string,
-  value?: string,
-  maxLength?: number,
-  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void,
-  onBlur?: () => void
-}
+  placeholder: string;
+  value?: string;
+  maxLength?: number;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  onBlur?: () => void;
+};
 
-export default function Input({placeholder, value, maxLength, onChange, onBlur}: Props) {
+export default function Input({
+  placeholder,
+  value,
+  maxLength,
+  onChange,
+  onBlur,
+}: Props) {
   return (
-    <input 
-      className='w-full h-full py-2 px-4 bg-white border border-neutral-300 shadow-sm rounded-full outline-none'
+    <input
+      className="w-full h-full py-3 px-4 bg-white border border-neutral-300 shadow-sm rounded-full outline-none"
       placeholder={placeholder}
       value={value ?? ""}
       maxLength={maxLength}
       onChange={onChange}
       onBlur={onBlur}
     />
-  )
+  );
 }
