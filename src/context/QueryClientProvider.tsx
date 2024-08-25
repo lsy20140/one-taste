ba0eccc88ebdc59b3236654e3c18ac20f9bdac48
lsy20140 <lsy20140@naver.com>
2024-08-25 18:59:00 +0900

    chore: QueryClient option 추가

--- src/context/QueryClientProvider.tsx
+++ src/context/QueryClientProvider.tsx
@@ -1,14 +1,23 @@
-'use client'
+"use client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-export const queryClient = new QueryClient()
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 1,
+      refetchOnMount: false,
+      refetchOnWindowFocus: false,
+      staleTime: 60 * 1000,
+    },
+  },
+});
 
-export default function QueryProvider({children}:{children: React.ReactNode}) {
-
+export default function QueryProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
-    <QueryClientProvider client={queryClient}>
-      {children}
-    </QueryClientProvider>
-
-  )
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
 }
