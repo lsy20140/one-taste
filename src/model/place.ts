8f9dcaeefd002290e7aa09795a86267373e93b85
lsy20140 <lsy20140@naver.com>
2024-09-14 23:12:09 +0900

    feat: JeboPlace 타입 추가

--- src/model/place.ts
+++ src/model/place.ts
@@ -1,45 +1,53 @@
 export type DetailPlace = {
-  place_id: Number;
-  name: string;
-  address: string;
-  content: string | null;
-  opening_hours: string | null;
-  closed_days: string | null;
-  phone: string | null;
-  dibs_list: string[];
-  cate_name: string;
-};
+  place_id: Number
+  name: string
+  address: string
+  content: string | null
+  opening_hours: string | null
+  closed_days: string | null
+  phone: string | null
+  dibs_list: string[]
+  cate_name: string
+}
 
-export type SimplePlace = Omit<DetailPlace, "comments">;
-export type AdminPlaceInfo = Omit<SimplePlace, "dibs_list">;
+export type SimplePlace = Omit<DetailPlace, "comments">
+export type AdminPlaceInfo = Omit<SimplePlace, "dibs_list">
 
 export type Comment = {
-  comment_id: Number;
-  content: string;
-  rest_id: Number;
-  user_id: string;
-  created_date: Date;
-};
+  comment_id: Number
+  content: string
+  rest_id: Number
+  user_id: string
+  created_date: Date
+}
 
 export type Image = {
-  image_id: Number;
-  rest_id: Number;
-  image_url: string;
-  created_date: Date;
-  user_id: string;
-  like_user_list: string[];
-};
+  image_id: Number
+  rest_id: Number
+  image_url: string
+  created_date: Date
+  user_id: string
+  like_user_list: string[]
+}
 
 export type OpeningHour = {
   [key: string]: {
-    time_range: string;
-    break_time: string;
-    last_order: string;
-  };
-};
+    time_range: string
+    break_time: string
+    last_order: string
+  }
+}
 
 export type JeboSearchPlace = {
-  title: string;
-  roadAddress: string;
-  category: string;
-};
+  title: string
+  roadAddress: string
+  category: string
+  link: string
+}
+
+export type JeboPlace = {
+  jebo_id: number
+  address: string
+  name: string
+  link: string
+}
