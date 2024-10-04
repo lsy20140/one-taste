eafc27c69473727c36122f0c1463529031f157c2
lsy20140 <lsy20140@naver.com>
2024-10-04 18:35:47 +0900

    design: 이미지 전체화면 top 여백 수정

--- src/components/FullImage/index.tsx
+++ src/components/FullImage/index.tsx
@@ -1,20 +1,24 @@
 'use client'
-import Image from "next/image";
-import { useRouter } from "next/navigation"
-import { IoClose } from "react-icons/io5"
+import Image from 'next/image'
+import { useRouter } from 'next/navigation'
+import { IoClose } from 'react-icons/io5'
 
 type Props = {
   url: string
 }
 
-export default function FullImage({url}: Props) {
+export default function FullImage({ url }: Props) {
   const router = useRouter()
   return (
     <>
-      <div className="absolute top-16 bg-black w-screen h-full flex justify-center">
-        <IoClose fontSize={32} className="absolute top-8 right-8 text-white cursor-pointer" onClick={() => router.back()} />
-        <div className="relative w-1/2 h-full">
-          <Image src={url} alt="" fill className="object-contain"/>
+      <div className='absolute top-14 bg-black w-screen h-full flex justify-center'>
+        <IoClose
+          fontSize={32}
+          className='absolute top-8 right-8 text-white cursor-pointer'
+          onClick={() => router.back()}
+        />
+        <div className='relative w-1/2 h-full'>
+          <Image src={url} alt='' fill className='object-contain' />
         </div>
       </div>
     </>
